35e705a96018a30a1529284f7f33d8c616ce23e3
saitrogen <ashiquvava381@gmail.com>
2025-06-19 18:20:48 +0530

    triger deployemnt changed to createwebhashistory

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { supabase } from '../lib/supabase'
 
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/login",
